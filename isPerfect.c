56346b1f3a931ac929a338bb1c152d393b25ed11
AnushaChintha <anusha.chintha00@gmail.com>
2019-05-19 15:57:06 +0530

    isnumber perfect

---
+++ isPerfect.c
@@ -0,0 +1,39 @@
+/*Problem - To find if given number is perfect or not
+
+Description - A perfect number is a number which has the sum of its factors equal to the number itself.For each test case, you will be given two arrays, an input array consisting of numbers and output array of - 1's of the same length. For every index, if the number in the input array is perfect then set the corresponding number in output array to 1. If not, then set the corresponding number to 0.
+
+Test Case 1:
+
+	Input = [6, 4];
+	Output = [-1, -1];
+	length = 2;
+
+	The factors of 6 are 1, 2, 3 whose sum equals 1 + 2 + 3 = 6. Hence 6 is perfect.
+		The factors of 4 are 1, 2 whose sum equals 1 + 2 = 3. Hence 4 is not perfect.
+
+		Hence output array must be[1, 0];
+
+		DONT CHANGE THE FUNCTION HEADERS.
+*/
+void isPerfect(long input[], int output[], int length)
+{
+	int farr[]=new int[10];
+	int index1, index2, sum, index3;
+	for (index1 = 0; index1 < length; index1++)
+	{  sum=0;
+
+		for (index2 = 1; index2 < input[index1]; index2++)
+		{
+			if (input[index1] % index2 == 0)
+				sum+=index2;
+		if (sum == input[index1])
+			output[index1] = 1;
+		else
+			output[index1] = 0;
+		}
+
+	}
+	for (index1 = 0; index1 <= length; index1++)
+	{	printf("%d",output[index1]);
+}
+}
